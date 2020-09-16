6e71cfe2de123c042e49e7df5574f5415fc4562f
Jonathan Tse <Jonathan@Jonathans-MBP.broadband>
2020-09-16 22:21:51 +0100

    correct routing now in place thanks to Link and NavLink imports inside Navbar component, excellent

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,18 +1,19 @@
 import React, { Component } from "react";
 import styles from "./Navbar.module.scss";
+import { Link, NavLink } from 'react-router-dom'
 
 class Navbar extends Component {
     render() {
         return (
             <nav>
                 <ul className={styles.navbar}>
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/about">About Me</a></li>
-                    <li><a href="/events">Events</a></li>
-                    <li><a href="/gallery">Gallery</a></li>
-                    <li><a href="/media">Media</a></li>
-                    <li><a href="/teaching">Teaching</a></li>
-                    <li><a href="/contact">Contact</a></li>
+                    <li><Link to="/">Home</Link></li>
+                    <li><NavLink to="/about">About Me</NavLink></li>
+                    <li><NavLink to="/events">Events</NavLink></li>
+                    <li><NavLink to="/gallery">Gallery</NavLink></li>
+                    <li><NavLink to="/media">Media</NavLink></li>
+                    <li><NavLink to="/teaching">Teaching</NavLink></li>
+                    <li><NavLink to="/contact">Contact</NavLink></li>
                 </ul>
             </nav>
         )
