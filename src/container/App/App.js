ebff4ed85167b51f83d4ca6f933347b8f4750639
Jonathan Tse <Jonathan@Jonathans-MBP.broadband>
2020-09-16 14:32:49 +0100

    created App container with App.module.scss file and got H1 rendering onto page as initial start up

---
+++ src/container/App/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import styles from './App.module.scss';
+
+class App extends Component {
+  render () {
+    return (
+      <div className={styles.App}>
+        <header className="App-header">
+          <h1>Louisa Lam</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
