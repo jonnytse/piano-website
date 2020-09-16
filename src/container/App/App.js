2d85e87749662492fe04885975d9ee67e9abf9b4
Jonathan Tse <Jonathan@Jonathans-MBP.broadband>
2020-09-16 16:09:02 +0100

    Started components creation and first component is header which renders correctly onto page

--- src/container/App/App.js
+++ src/container/App/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
 import styles from './App.module.scss';
+import Header from '../../components/Header';
 
 class App extends Component {
   render () {
     return (
       <div className={styles.App}>
-        <header className="App-header">
-          <h1>Louisa Lam</h1>
-        </header>
+        <Header />
       </div>
     );
   }
