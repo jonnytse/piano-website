7b30309d9c10ca8dbf607d23ccc701be9ea6b6f6
Jonathan Tse <Jonathan@Jonathans-MBP.broadband>
2020-09-18 14:11:17 +0100

    added a red (or gold) hyperlink for BBC Young Musician in About component and also added YouTube videos in Media component

--- src/components/Media/Media.jsx
+++ src/components/Media/Media.jsx
@@ -7,6 +7,9 @@ class Media extends Component {
             <div className={styles.media}>
                 <h4>Media</h4>
                 <p>YouTube links of my playing</p>
+                <iframe title="Bridge Over Troubled Water" width="560" height="315" src="https://www.youtube.com/embed/9ojF86BhMk4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <iframe title="Boogie Woogie Bugle Boy" width="560" height="315" src="https://www.youtube.com/embed/XEBFziYOShY" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <iframe title="Lost Chord Dementia Charity" width="560" height="315" src="https://www.youtube.com/embed/YXwcVqQafio" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             </div>
         )
     }
