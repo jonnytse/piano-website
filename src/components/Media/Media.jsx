96bad09b5162f44e17f6df7718af8924dd0efc5b
Jonathan Tse <Jonathan@Jonathans-MBP.broadband>
2020-09-18 17:30:03 +0100

    Tidied up YouTube videos in Media component and added BBC Young Musician YouTube video in About component

--- src/components/Media/Media.jsx
+++ src/components/Media/Media.jsx
@@ -8,9 +8,10 @@ class Media extends Component {
                 <h4>Media</h4>
                 {/* <p>YouTube links of my playing</p> */}
                 <div className={styles.iframecontainer}>
-                    <iframe title="Bridge Over Troubled Water" width="616" height="346.5" src="https://www.youtube.com/embed/9ojF86BhMk4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br/>
-                    <iframe title="Boogie Woogie Bugle Boy" width="616" height="346.5" src="https://www.youtube.com/embed/XEBFziYOShY" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br/>
-                    <iframe title="Lost Chord Dementia Charity" width="616" height="346.5" src="https://www.youtube.com/embed/YXwcVqQafio" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    {/* 560/315 */}
+                    <iframe title="Bridge Over Troubled Water" width="560" height="315" src="https://www.youtube.com/embed/9ojF86BhMk4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br/>
+                    <iframe title="Boogie Woogie Bugle Boy" width="560" height="315" src="https://www.youtube.com/embed/XEBFziYOShY" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br/>
+                    <iframe title="Lost Chord Dementia Charity" width="560" height="315" src="https://www.youtube.com/embed/YXwcVqQafio" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 </div>
             </div>
         )
