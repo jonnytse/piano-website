92832fffc0d6848c4e15f624d2d24d4a592ec638
Jonathan Tse <Jonathan@Jonathans-MBP.broadband>
2020-09-18 16:47:50 +0100

    YouTube videos are now responsive to browser window changes

--- src/components/Media/Media.jsx
+++ src/components/Media/Media.jsx
@@ -7,10 +7,10 @@ class Media extends Component {
             <div className={styles.media}>
                 <h4>Media</h4>
                 {/* <p>YouTube links of my playing</p> */}
-                <div className={styles.iframe}>
-                    <iframe title="Bridge Over Troubled Water" width="560" height="315" src="https://www.youtube.com/embed/9ojF86BhMk4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br/>
-                    <iframe title="Boogie Woogie Bugle Boy" width="560" height="315" src="https://www.youtube.com/embed/XEBFziYOShY" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br/>
-                    <iframe title="Lost Chord Dementia Charity" width="560" height="315" src="https://www.youtube.com/embed/YXwcVqQafio" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <div className={styles.iframecontainer}>
+                    <iframe title="Bridge Over Troubled Water" width="616" height="346.5" src="https://www.youtube.com/embed/9ojF86BhMk4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br/>
+                    <iframe title="Boogie Woogie Bugle Boy" width="616" height="346.5" src="https://www.youtube.com/embed/XEBFziYOShY" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br/>
+                    <iframe title="Lost Chord Dementia Charity" width="616" height="346.5" src="https://www.youtube.com/embed/YXwcVqQafio" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 </div>
             </div>
         )
