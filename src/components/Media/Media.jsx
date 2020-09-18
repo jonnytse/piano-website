d521ffdebfa35cff4dea26a0738914eb9f1c0d9c
Jonathan Tse <Jonathan@Jonathans-MBP.broadband>
2020-09-18 22:46:03 +0100

    made Youtube video frames slightly smaller in Media and About components

--- src/components/Media/Media.jsx
+++ src/components/Media/Media.jsx
@@ -9,9 +9,9 @@ class Media extends Component {
                 {/* <p>YouTube links of my playing</p> */}
                 <div className={styles.iframecontainer}>
                     {/* 560/315 */}
-                    <iframe title="Bridge Over Troubled Water" width="560" height="315" src="https://www.youtube.com/embed/9ojF86BhMk4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br/>
-                    <iframe title="Boogie Woogie Bugle Boy" width="560" height="315" src="https://www.youtube.com/embed/XEBFziYOShY" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br/>
-                    <iframe title="Lost Chord Dementia Charity" width="560" height="315" src="https://www.youtube.com/embed/YXwcVqQafio" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <iframe title="Bridge Over Troubled Water" width="504" height="283" src="https://www.youtube.com/embed/9ojF86BhMk4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br/>
+                    <iframe title="Boogie Woogie Bugle Boy" width="504" height="283" src="https://www.youtube.com/embed/XEBFziYOShY" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br/>
+                    <iframe title="Lost Chord Dementia Charity" width="504" height="283" src="https://www.youtube.com/embed/YXwcVqQafio" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 </div>
             </div>
         )
